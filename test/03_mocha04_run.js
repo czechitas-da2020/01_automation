1e77bb028ce9c8d37024989af5d0d1d4b6c59141
Vojtěch Červený <cervik49@gmail.com>
2020-11-10 03:04:26 +0100

    showCases

---
+++ test/03_mocha04_run.js
@@ -0,0 +1,30 @@
+// ukazka jak muze vypadat VICE testcasu jedne "feature" / stranky
+
+// CO BUDEME TESTOVAT
+// describe('Page automation selectors', () => {
+
+
+//     describe('Section ZADANI', ()=>{
+//         it('has 8points what to do', ()=>{})
+//         it('has gray background', ()=>{})
+//     })
+
+//     describe('section HINT', ()=>{
+
+//         it('has correct text', ()=>{})
+//         it('has light yellow background', ()=>{})
+//     }) 
+// });
+
+describe('section fairytale', () => {
+    beforeEach(() => {
+        console.log('BEFORE EVERY TESTS')
+    })
+
+    it('has correct sum of paragraphs', () => {
+        console.log('TESTCASE - has correct sum of paragraphs')
+    })
+    it('has correct text', () => {
+        console.log('TESTCASE - has correct text')
+    })
+})
